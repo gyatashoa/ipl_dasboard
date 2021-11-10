1ce6f3cba65e58b41797c4a654bec8183a5c0f7e
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2021-11-10 10:11:54 +0000

    initial commit

---
+++ src/main/java/me/felixcodes/ipl_dasboard/IplDasboardApplication.java
@@ -0,0 +1,13 @@
+package me.felixcodes.ipl_dasboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDasboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDasboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/felixcodes/ipl_dasboard/IplDasboardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.felixcodes.ipl_dasboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDasboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
